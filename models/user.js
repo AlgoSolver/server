d1e102ae6ef71150cc950066e660e869af012fad
s403o <eslam.adel@protonmail.com>
2021-01-19 21:41:40 +0200

    user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,45 @@
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
+const User = mongoose.model('User', new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 10,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    status: ["admin", "student", "content_creator"]
+  },
+  timestamps: true,
+}));
 
-const userSchema = mongoose.Schema({},{
-  timestamps:true
-})
+function validateUser(user) {
+  const schema = {
+    username: Joi.string().min(5).max(10).required(),
+    password: Joi.string().min(8).max(20).required().regex(/^[\w]{8,20}$/),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    role: Joi.string().required()
+  };
 
-module.exports= mongoose.model('User',userSchema)
+  return Joi.validate(customer, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
