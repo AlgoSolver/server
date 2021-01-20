b4f80c64393db03de2018604519c487520e4df5b
Islam Mostafa <islamghany3@gmail.com>
2021-01-20 13:04:55 +0200

    adding the user's relationships

--- models/user.js
+++ models/user.js
@@ -27,6 +27,17 @@ const User = mongoose.model('User', new mongoose.Schema({
     required: true,
     status: ["admin", "student", "content_creator"]
   },
+  /*
+  adding the user relationships 
+
+  blogs:[{type:mongoose.ObjectID,ref:"blog"}],
+  bookmarkedBlogs:[{type:mongoose.ObjectID,ref:"blog"}],
+
+  solvedProblems:[{type:mongoose.ObjectID,ref:"Problem"}],
+  bookmarkedProblems:[{type:mongoose.ObjectID,ref:"Problem"}],
+
+  codes:[{type:mongoose.ObjectID,ref:"Code"}],
+  */
 }, {timestamps: true}));
 
 function validateUser(user) {
