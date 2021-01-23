73f4987c36da8567cc1177192be87c01189940a3
s403o <eslam.adel@protonmail.com>
2021-01-23 21:05:25 +0200

    fix track & add validaton structure

--- models/track.js
+++ models/track.js
@@ -24,16 +24,27 @@ const trackSchema = new Schema(
     parentTrack: {
       type: String,
       required: true
-    }
+    },
     prerequistes: {
       type: String,
       required: true
     }
   },
   { timestamps: true }
-  
 );
 
- 
+function validateTrack(track) {
+  const schema = {
+    title: Joi.string().min(10).max(50).required(),
+    content: Joi.string().min(8).max(20).required(),
+    author: Joi.string().required(),
+    childrenTracks: Joi.string().required(),
+    parentTrack: Joi.string().required(),
+    prerequistes: Joi.string().required()
+  };
 
+  return Joi.validate(track, schema);
+}
+ 
+exports.validate = validateTrack;
 module.exports = mongoose.model('Track', trackSchema);
