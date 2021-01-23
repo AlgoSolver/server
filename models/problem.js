5e334fa46fbef71c2937d768485124ed953b9440
Amir Haytham Salama <amir.haytham.salama@gmail.com>
2021-01-23 08:21:21 +0200

    Added joi & testCaseSchema & validateProblemSchema

--- models/problem.js
+++ models/problem.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 const Schema = mongoose.Schema;
+
+const testCaseSchema = new mongoose.Schema(
+  {
+    input: String,
+    output: String,
+  },
+ {timestamps: true},
+);
 
 const problemSchema = new Schema({
   description: {
@@ -14,7 +23,25 @@ const problemSchema = new Schema({
     editorial: {required: true, type: String},
   },
   
-}, {timestamps: true});
+}, {timestamps: true}
+);
 
+function validateProblem(Problem) {
+  const schema = {
+     title: Joi.string().required(),
+      statement: Joi.string().required(),
+      sampleInput: Joi.string().required(),
+      sampleOutput: Joi.string().required(),
+      samples: Joi.string().required(),
+      timeLimit: Joi.string().required(),
+      memoryLimit: Joi.string().required(),
+      modelAnswer: Joi.string().required(),
+      editorial: Joi.string().required(),
+  };
+
+  return Joi.validate(user, schema);
+}
 
 module.exports = mongoose.model('Problem', problemSchema);
+module.exports = mongoose.model('testCaseSchema', testCaseSchema);
+module.exports = mongoose.model('validateProblem', validateProblem);
