4c4c74074378bb617b1418618c5787d3c01ad3c9
khaledrezk <khaledkra2112@gmail.com>
2021-01-29 05:03:44 +0200

    Implemented the mongoose schema for TestSets

--- models/problem.js
+++ models/problem.js
@@ -1,4 +1,11 @@
+const mongoose = require("mongoose");
 const Joi = require("joi");
+const { func } = require("joi");
+
+
+mongoose.connect("mongodb://localhost/playground")// only here for testing will be removed and implemented at index.js
+    .then(() => console.log("Connected Successfully..."))
+    .catch((err) => console.error(err));
 
 
 function validateTestSet(testSet){
@@ -11,10 +18,28 @@ function validateTestSet(testSet){
     return error;
 }
 
-
-
-
-
+const testSetSchema = new mongoose.Schema({
+    constraints: {
+        type: String,
+        required : true
+    },
+    expectedComplexity : {
+        type : String,
+        default: "Can't detect Complexity"// for problems where we can't decide the exact complexity such as randomization problems also for sample input
+    }, 
+    testCases: {
+        type : [String],
+        validate: {
+            validator: function(tests){
+                if(tests && Array.isArray(tests) && tests.length >= 1){
+                    return true;
+                }
+                return false;
+            },
+            message: "TestCases should be an array of strings of size at least 1"
+        }
+    }
+});
 
 
 module.exports.validateTestSet = validateTestSet;
