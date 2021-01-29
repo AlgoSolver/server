31dc7fbdc9a3305a4331b40f7963d6eb4a8415f9
khaledrezk <khaledkra2112@gmail.com>
2021-01-29 15:46:11 +0200

    Implemented the mongoose schema for Problem

--- models/problem.js
+++ models/problem.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const { func } = require("joi");
+const User = require("./user.js");
+const Code = require("./code");
 
 
 mongoose.connect("mongodb://localhost/playground")// only here for testing will be removed and implemented at index.js
@@ -41,5 +42,93 @@ const testSetSchema = new mongoose.Schema({
     }
 });
 
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const problemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: ObjectId,
+        ref: "User",
+        required: true
+    },
+    timeLimit: {
+        type: Number,
+        default: 1,
+        min: .5,
+        max: 10,
+        required: true
+    },
+    memoryLimit: {
+        type: Number,
+        default: 1024*256, //256 MB
+        min: 1024* 16, 
+        max: 1024*1024,// maximum memory is 1GB
+        required: true
+    },
+    editorial: {// should be required to publish problem? not sure
+        type: String
+    },
+    testSets: {
+        type: [testSetSchema],
+        required: true,
+        validate: {
+            validator: function (tests) {
+                if( this.isPublished && (!tests || !Array.isArray(tests) || tests.length == 0))
+                    return false;
+                return true;// can be empty if not published
+            },
+            message: "testSets should be an array of testsets with at least one valid testSet"
+        }
+    },
+    isPublished : {
+        type : Boolean,
+        default: false,
+        validate:{
+            validator: function(value){
+                console.log("Calling validator of is published \n This is :", this, "\n\n\n");
+                if(!value)
+                    return true;
+                if(!this.modelAnswer || !this.checker || !this.validator || !this.testSets || !Array.isArray(this.testSets) || this.testSets.length == 0)
+                    return false;
+                return true;
+            },
+            message: "All problem data should be completed before publishing"// replace with proper function in the future
+        }
+        // also when play ground is hosted we should add validation that checker, code are correct.
+    },
+    modelAnswer: {// should be required before publishing problem
+        type: ObjectId, 
+        ref: "Code",
+        required: function () {// if is published then code should be required
+            return this.isPublished;
+        }
+    },
+    checker: {// should be required before publishing problem
+        type: ObjectId, 
+        ref: "Code",
+        required: function () {// if is published then code should be required
+            return this.isPublished;
+        }
+    },
+    validator: {// should be required before publishing problem
+        type: ObjectId, 
+        ref: "Code",
+        required: function () {// if is published then code should be required
+            return this.isPublished;
+        }
+    }
+    
+});
+
+const Problem = new mongoose.model("Problem", problemSchema);
+
 
 module.exports.validateTestSet = validateTestSet;
+module.exports.Problem = Problem;
