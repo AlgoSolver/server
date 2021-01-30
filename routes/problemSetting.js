f3038cd0c1d10b0c9c512d54c0f98d67ed8ff489
khaledrezk <khaledkra2112@gmail.com>
2021-01-30 02:08:39 +0200

    Implemented initial getProblems endpoint

--- routes/problemSetting.js
+++ routes/problemSetting.js
@@ -6,4 +6,16 @@ router.get("/", (req, res) => {
     res.send({message : "Welcome Our Tourist :) !!!"});
 })
 
+router.get("/getProblems", async(req, res) => {
+    try{
+        const problems = await getProblems();
+        res.send(problems);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send({message : "Sorry we are facing an internal error please try again later ..."})
+    }
+});
+
+
 module.exports = router;
