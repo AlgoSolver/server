81847cde9266efa71be2fcfe9362898814b768bd
khaledrezk <khaledkra2112@gmail.com>
2021-01-31 08:12:38 +0200

    updated the buggy implementation of code module

--- models/code.js
+++ models/code.js
@@ -1,20 +1,16 @@
-const { Buffer } = require('buffer');
 const Joi = require('joi');
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require('mongoose');
 
-const Code = mongoose.model('Code', new mongoose.Schema({
-  codeId: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  userId: {
+const Code = new mongoose.model('Code', new mongoose.Schema({
+  author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
-  source_code: {
-    data: String, // to store our sourcce as data in the form of arrays.
+  sourceCode: {
+    type : String, // to store our source code
+    required : true
   },
   language: {
     type: String,
@@ -23,12 +19,35 @@ const Code = mongoose.model('Code', new mongoose.Schema({
 }, {timestamps: true}));
 
 function validateCode(code) {
-  const schema = {
-    codeId: Joi.number().unique().required(),
-  };
+  const schema = Joi.object({
+    author: Joi.objectId().required(),
+    sourceCode : Joi.string().required(),
+    language : Joi.string().required().equal("C++")
+  });
 
-  return Joi.validate(code, schema);
+  return schema.validate(code);
 }
+
+const {error} = validateCode({
+  author : "123456789012123456789012",
+  sourceCode : "Cout << \" Hello World \" << endl;\n",
+  language : "C++"
+
+});
+console.log("validation error => ", error);
+
+// createCode({
+//   author : "60158b8029b9b6160c5f078b",
+//   sourceCode: "#include<bits/stdc++.h>\n\nusing namespace std;\n\nint main(){\n cout << \"hello World\" << endl;\n  return 0;\n}\n",
+//   language : "C++"
+// }).then(code => console.log("Code :", code))
+//   .catch(err => console.error(err));
+
+// async function printCodes() {
+//   const codes = await Code.find();
+//   console.log("Codes => ", codes);
+// }
+// printCodes();
 
 exports.Code = Code;
 exports.validate = validateCode;
