5705ed3d58f8c707784dd563c09a5dd53cb27971
s403o <eslam.adel.me@gmail.com>
2021-04-23 04:47:36 +0200

    fix SyntaxError!

--- src/resources/_global-middlewares/auth-guard.js
+++ src/resources/_global-middlewares/auth-guard.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-	if (req?.auth?.notAuth === true || !req.auth) {
+	if (req.auth.notAuth === true || !req.auth) {
 		return res
 			.status(401)
 			.json({ message: "Unauthrized" });
